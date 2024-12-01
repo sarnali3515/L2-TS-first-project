c6fd8fc5a3e2e8f52ae8e40108edcfbc66e88004
sarnali15 <191sarnali@gmail.com>
2024-12-02 00:27:23 +0600

    semester controller and service created

---
+++ src/app/modules/academicSemester/academicSemester.controller.ts
@@ -0,0 +1,21 @@
+import sendResponse from '../../utils/sendResponse';
+import status from 'http-status';
+import catchAsync from '../../utils/catchAsync';
+import { AcademicSemesterServices } from './academicSemester.service';
+
+const createAcademicSemester = catchAsync(async (req, res) => {
+  const result = await AcademicSemesterServices.createAcademicSemesterIntoDB(
+    req.body,
+  );
+
+  sendResponse(res, {
+    statusCode: status.OK,
+    success: true,
+    message: 'Academic Semester is created successfully',
+    data: result,
+  });
+});
+
+export const AcademicSemesterControllers = {
+  createAcademicSemester,
+};
---
+++ src/app/modules/academicSemester/academicSemester.service.ts
@@ -0,0 +1,12 @@
+import { TAcademicSemester } from './academicSemester.interface';
+import { AcademicSemester } from './academicSemester.model';
+
+const createAcademicSemesterIntoDB = async (payload: TAcademicSemester) => {
+  const result = await AcademicSemester.create(payload);
+
+  return result;
+};
+
+export const AcademicSemesterServices = {
+  createAcademicSemesterIntoDB,
+};
