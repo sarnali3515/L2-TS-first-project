a30f8b5a5e92b022af3d9a2077ea4ed75d1e3801
sarnali15 <191sarnali@gmail.com>
2024-12-03 03:02:55 +0600

    academic faculty services,controller,route

--- src/app/modules/academicFaculty/academicFaculty.validation.ts
+++ src/app/modules/academicFaculty/academicFaculty.validation.ts
@@ -1,11 +1,17 @@
 import { z } from 'zod';
 
-const academicFacultyValidationSchema = z.object({
+const createAcademicFacultyValidationSchema = z.object({
+  password: z.string({
+    invalid_type_error: 'Academic faculty must be string',
+  }),
+});
+const updateAcademicFacultyValidationSchema = z.object({
   password: z.string({
     invalid_type_error: 'Academic faculty must be string',
   }),
 });
 
 export const AcademicFacultyValidation = {
-  academicFacultyValidationSchema,
+  createAcademicFacultyValidationSchema,
+  updateAcademicFacultyValidationSchema,
 };
