f19e4c55418bcb044f2051c7fd1327bf4be6d58a
sarnali15 <191sarnali@gmail.com>
2024-12-15 23:14:37 +0600

    transaction added

--- src/app/modules/Course/course.service.ts
+++ src/app/modules/Course/course.service.ts
@@ -1,7 +1,10 @@
+import mongoose from 'mongoose';
 import QueryBuilder from '../../builder/QueryBuilder';
 import { CourseSearchableFields } from './course.constant';
 import { TCourse } from './course.interface';
 import { Course } from './course.model';
+import AppError from '../../errors/AppError';
+import status from 'http-status';
 
 const createCourseIntoDB = async (payload: TCourse) => {
   const result = await Course.create(payload);
@@ -32,41 +35,82 @@ const getSingleCourseFromDB = async (id: string) => {
 const updateCourseIntoDB = async (id: string, payload: Partial<TCourse>) => {
   const { preRequisiteCourses, ...courseRemainingData } = payload;
 
-  const updateBasicCourseInfo = await Course.findByIdAndUpdate(
-    id,
-    courseRemainingData,
-    {
-      new: true,
-      runValidators: true,
-    },
-  );
+  const session = await mongoose.startSession();
+  try {
+    session.startTransaction();
 
-  // check if there is any prerequisite courses to update
-  if (preRequisiteCourses && preRequisiteCourses.length > 0) {
-    //filter out the deleted fields
-    const deletedPreRequisites = preRequisiteCourses
-      .filter((el) => el.course && el.isDeleted)
-      .map((el) => el.course);
-
-    const deletedPreRequisiteCourses = await Course.findByIdAndUpdate(id, {
-      $pull: { preRequisiteCourses: { course: { $in: deletedPreRequisites } } },
-    });
-
-    // filter out the new course fields
-    const newPreRequisites = preRequisiteCourses?.filter(
-      (eL) => eL.course && !eL.isDeleted,
+    const updateBasicCourseInfo = await Course.findByIdAndUpdate(
+      id,
+      courseRemainingData,
+      {
+        new: true,
+        runValidators: true,
+        session,
+      },
     );
 
-    const newPreRequisiteCourses = await Course.findByIdAndUpdate(id, {
-      $addToSet: { preRequisiteCourses: { $each: newPreRequisites } },
-    });
+    if (!updateBasicCourseInfo) {
+      throw new AppError(status.BAD_REQUEST, 'Failed to update course');
+    }
+
+    // check if there is any prerequisite courses to update
+    if (preRequisiteCourses && preRequisiteCourses.length > 0) {
+      //filter out the deleted fields
+      const deletedPreRequisites = preRequisiteCourses
+        .filter((el) => el.course && el.isDeleted)
+        .map((el) => el.course);
+
+      const deletedPreRequisiteCourses = await Course.findByIdAndUpdate(
+        id,
+        {
+          $pull: {
+            preRequisiteCourses: { course: { $in: deletedPreRequisites } },
+          },
+        },
+        {
+          new: true,
+          runValidators: true,
+          session,
+        },
+      );
+
+      // filter out the new course fields
+      const newPreRequisites = preRequisiteCourses?.filter(
+        (eL) => eL.course && !eL.isDeleted,
+      );
+
+      if (!deletedPreRequisiteCourses) {
+        throw new AppError(status.BAD_REQUEST, 'Failed to update course');
+      }
+
+      const newPreRequisiteCourses = await Course.findByIdAndUpdate(
+        id,
+        {
+          $addToSet: { preRequisiteCourses: { $each: newPreRequisites } },
+        },
+        {
+          new: true,
+          runValidators: true,
+          session,
+        },
+      );
+      if (!newPreRequisiteCourses) {
+        throw new AppError(status.BAD_REQUEST, 'Failed to update course');
+      }
+      const result = await Course.findById(id).populate(
+        'preRequisiteCourses.course',
+      );
+
+      return result;
+    }
+
+    await session.commitTransaction();
+    await session.endSession();
+  } catch {
+    await session.abortTransaction();
+    await session.endSession();
+    throw new AppError(status.BAD_REQUEST, 'Failed to update course');
   }
-
-  const result = await Course.findById(id).populate(
-    'preRequisiteCourses.course',
-  );
-
-  return { result };
 };
 
 const deleteCourseFromDB = async (id: string) => {
