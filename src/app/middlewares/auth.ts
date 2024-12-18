c6c6d0e1fc7a24007799aecbb1c086f4df05af33
sarnali15 <191sarnali@gmail.com>
2024-12-18 20:35:59 +0600

    authorization with auth

--- src/app/middlewares/auth.ts
+++ src/app/middlewares/auth.ts
@@ -4,8 +4,9 @@ import catchAsync from '../utils/catchAsync';
 import AppError from '../errors/AppError';
 import jwt, { JwtPayload } from 'jsonwebtoken';
 import config from '../config';
+import { TUserRole } from '../modules/User/user.interface';
 
-const auth = () => {
+const auth = (...requiredRoles: TUserRole[]) => {
   return catchAsync(async (req: Request, res: Response, next: NextFunction) => {
     const token = req.headers.authorization;
     // console.log(token);
@@ -21,6 +22,12 @@ const auth = () => {
       config.jwt_access_secret as string,
       function (err, decoded) {
         if (err) {
+          throw new AppError(httpStatus.UNAUTHORIZED, 'You are not authorized');
+        }
+
+        const role = (decoded as JwtPayload).role;
+
+        if (requiredRoles && !requiredRoles.includes(role)) {
           throw new AppError(httpStatus.UNAUTHORIZED, 'You are not authorized');
         }
 
