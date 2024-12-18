505d93e4e6c084ff732ba9772abba27f29740df8
sarnali15 <191sarnali@gmail.com>
2024-12-18 19:46:22 +0600

    auth middleware added

--- src/app/middlewares/validateRequest.ts
+++ src/app/middlewares/validateRequest.ts
@@ -1,19 +1,16 @@
 import { NextFunction, Request, Response } from 'express';
 import { AnyZodObject } from 'zod';
+import catchAsync from '../utils/catchAsync';
 
 const validateRequest = (schema: AnyZodObject) => {
-  return async (req: Request, res: Response, next: NextFunction) => {
-    try {
-      // validation
-      //if allright next() ->
-      await schema.parseAsync({
-        body: req.body,
-      });
-      next();
-    } catch (err) {
-      next(err);
-    }
-  };
+  return catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+    // validation
+    //if allright next() ->
+    await schema.parseAsync({
+      body: req.body,
+    });
+    next();
+  });
 };
 
 export default validateRequest;
