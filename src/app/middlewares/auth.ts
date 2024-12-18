d9040287429e13bcd46957ed5184775dd54f6c17
sarnali15 <191sarnali@gmail.com>
2024-12-18 20:10:25 +0600

    verify token

--- src/app/middlewares/auth.ts
+++ src/app/middlewares/auth.ts
@@ -2,16 +2,32 @@ import { NextFunction, Request, Response } from 'express';
 import httpStatus from 'http-status';
 import catchAsync from '../utils/catchAsync';
 import AppError from '../errors/AppError';
+import jwt, { JwtPayload } from 'jsonwebtoken';
+import config from '../config';
 
 const auth = () => {
   return catchAsync(async (req: Request, res: Response, next: NextFunction) => {
     const token = req.headers.authorization;
-    console.log(token);
+    // console.log(token);
 
+    // checking if the token is missing
     if (!token) {
       throw new AppError(httpStatus.UNAUTHORIZED, 'You are not authorized!');
     }
-    next();
+
+    // checking if the given token is valid
+    jwt.verify(
+      token,
+      config.jwt_access_secret as string,
+      function (err, decoded) {
+        if (err) {
+          throw new AppError(httpStatus.UNAUTHORIZED, 'You are not authorized');
+        }
+
+        req.user = decoded as JwtPayload;
+        next();
+      },
+    );
   });
 };
 
